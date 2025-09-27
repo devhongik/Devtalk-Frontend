12a9069b34240363ac2462aa274a4be5cd674e38
wlsldm <lavesujin@gmail.com>
2025-09-28 05:01:53 +0900

    #59 [UI] 세미나 디테일 ui  구현

--- src/components/Semina/SeminaDetailCard.tsx
+++ src/components/Semina/SeminaDetailCard.tsx
@@ -1,11 +1,25 @@
 function SeminaDeailCard({ id }: { id: number }) {
   return (
     <div className="w-[375px] h-[449px] gap-20 p-20 flex flex-col">
-      <div className="w-[335px] h-[404px] gap-31 flex flex-col items-center jusitfy-center">
+      <div className="w-[335px] h-[404px] gap-[31px] flex flex-col">
         <div className="h-[68px] flex flex-col gap-8 justify-between">
           <div className="subhead-2-medium text-grey-100">{id}회차</div>
-          <div className="text"></div>
+          <div className="heading-2-bold text-gradient">LLM을 파헤치다</div>
         </div>
+        <img src="/" alt="seminar" className="h-[220px] rounded-8 border" />
+        <div className="h-[54px] flex flex-col gap-8 body-1-medium">
+          <div className="flex flex-row gap-28">
+            <div className="text-grey-300">일정</div>
+            <div className="text-grey-400">2025/ 4/ 30(수) 오후 6:30</div>
+          </div>
+          <div className="flex flex-row gap-28">
+            <div className="text-grey-300">장소</div>
+            <div className="text-grey-400">홍익대학교 L0201</div>
+          </div>
+        </div>
+      </div>
+      <div className="w-[102px] h-[25px] gap-10 px-8 py-4 rounded-4 text-gradient caption-semibold bg-grey-900">
+        발표자료 다운로드
       </div>
     </div>
   );
--- src/pages/user/seminar/Detail.tsx
+++ src/pages/user/seminar/Detail.tsx
@@ -1,11 +1,41 @@
 import { useParams } from 'react-router-dom';
 import Header from '../../../components/common/Header';
+import SeminaDeailCard from '../../../components/Semina/SeminaDetailCard';
+import { LectureCardSession } from '../../../components/LectureCard/LectureCardSession';
+import { LectureCardMain } from '../../../components/LectureCard/LectureCardMain';
+import ReviewCard from '../../../components/common/ReviewCard';
+import Cta from '../../../components/common/Cta';
 
 function SeminarDetail() {
   const { id } = useParams();
   return (
     <div>
-      <Header />
+      <div className="flex flex-col gap-32">
+        <Header />
+        <SeminaDeailCard id={Number(id)} />
+        <div className="w-[375px] h-[2170px] flex flex-col gap-24 px-20">
+          <div className="heading-3-semibold text-white">연사 소개</div>
+          <div className="flex flex-col gap-10 justify-center items-center bg-black">
+            <LectureCardMain />
+            <LectureCardSession />
+
+            <LectureCardMain />
+            <LectureCardSession />
+          </div>
+        </div>
+        <div className="h-[475px] gap-12 px-20 flex flex-col">
+          <div className="heading-3-semi-bold tesxt-white">후기</div>
+          <div className="w-[335px] h-[435px] flex flex-col gap-12">
+            <ReviewCard />
+            <ReviewCard />
+            <ReviewCard />
+          </div>
+        </div>
+        <div className="h-[152px]"></div>
+      </div>
+      <div className="fixed bottom-0">
+        <Cta />
+      </div>
     </div>
   );
 }
