dc4fdb3aa27c3695c3a143e73758c62f9e8a2fdd
wlsldm <lavesujin@gmail.com>
2025-09-27 11:14:15 +0900

    #59 [UI] 세미나 디테일 카드 ui 구현

--- src/components/Semina/SeminaDetailCard.tsx
+++ src/components/Semina/SeminaDetailCard.tsx
@@ -1,5 +1,14 @@
-function SeminaDeailCard() {
-  return <></>;
+function SeminaDeailCard({ id }: { id: number }) {
+  return (
+    <div className="w-[375px] h-[449px] gap-20 p-20 flex flex-col">
+      <div className="w-[335px] h-[404px] gap-31 flex flex-col items-center jusitfy-center">
+        <div className="h-[68px] flex flex-col gap-8 justify-between">
+          <div className="subhead-2-medium text-grey-100">{id}회차</div>
+          <div className="text"></div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default SeminaDeailCard;
--- src/pages/user/seminar/Detail.tsx
+++ src/pages/user/seminar/Detail.tsx
@@ -1,11 +1,11 @@
 import { useParams } from 'react-router-dom';
+import Header from '../../../components/common/Header';
 
 function SeminarDetail() {
   const { id } = useParams();
   return (
     <div>
-      <h1>세미나 상세 정보</h1>
-      <p>세미나 ID: {id}</p>
+      <Header />
     </div>
   );
 }
