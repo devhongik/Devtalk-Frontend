5e56549d839938f07401202f5b8fc7c80b1397a7
jungyungee <jungyungee0610@gmail.com>
2025-09-28 13:34:26 +0900

    [UI] 기본 설정 추가

---
+++ .vite/deps/react_jsx-dev-runtime.js
@@ -0,0 +1,22 @@
+import {
+  __commonJS
+} from "./chunk-BUSYA2B4.js";
+
+// node_modules/react/cjs/react-jsx-dev-runtime.development.js
+var require_react_jsx_dev_runtime_development = __commonJS({
+  "node_modules/react/cjs/react-jsx-dev-runtime.development.js"() {
+  }
+});
+
+// node_modules/react/jsx-dev-runtime.js
+var require_jsx_dev_runtime = __commonJS({
+  "node_modules/react/jsx-dev-runtime.js"(exports, module) {
+    if (false) {
+      module.exports = null;
+    } else {
+      module.exports = require_react_jsx_dev_runtime_development();
+    }
+  }
+});
+export default require_jsx_dev_runtime();
+//# sourceMappingURL=react_jsx-dev-runtime.js.map
--- src/pages/admin/home-manage/Reviews.tsx
+++ src/pages/admin/home-manage/Reviews.tsx
@@ -1,7 +1,10 @@
 const Reviews = () => {
   return (
-    <div>
-      <h1>후기관리</h1>
+    <div className="space-y-40 ml-60 mr-60 mb-[175px]">
+      <h1 className="mt-60 heading-1-bold text-white">후기 관리</h1>
+      <div className="w-full max-w-[1030px] min-w-[650px] mx-auto bg-grey-900 p-6 rounded-10 space-y-24">
+        <h2 className="heading-2-bold text-white mb-24">문의하기 링크</h2>
+      </div>
     </div>
   );
 };
