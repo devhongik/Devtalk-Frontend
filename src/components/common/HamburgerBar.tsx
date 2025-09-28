eda87996a515c742bea20efa53b6dbe4c56d1310
chaeyoon <codbs0722@gmail.com>
2025-09-28 22:45:53 +0900

    #76 [UI] 햄버거바 애니메이션 + navigate

--- src/components/common/HamburgerBar.tsx
+++ src/components/common/HamburgerBar.tsx
@@ -1,13 +1,26 @@
+import { useNavigate } from 'react-router-dom';
 import deleteIcon from '../../assets/icons/common/delete.svg';
 
-const HamburgerBar = ({ onClose }: { onClose: () => void }) => {
+type HamburgerBarProps = {
+  isOpen: boolean;
+  onClose: () => void;
+};
+
+const HamburgerBar = ({ isOpen, onClose }: HamburgerBarProps) => {
+  const navigate = useNavigate();
+
   return (
     <>
       {/* 탭 뒤 화면 오버레이 */}
-      <div className="fixed inset-0 bg-black/30 z-40" onClick={onClose} />
+      <div
+        className={`fixed inset-0 bg-black/30 z-40 transition-opacity duration-300 ${isOpen ? 'opacity-100' : 'opacity-0 pointer-events-none'}`}
+        onClick={onClose}
+      />
 
       {/* 햄버거 바 */}
-      <div className="absolute top-0 right-0 w-[294px] h-[812px] bg-black z-50 transform transition-all duration-200">
+      <div
+        className={`fixed top-0 right-0 w-[294px] h-[852px] bg-black z-50 transform transition-transform duration-300 ${isOpen ? 'translate-x-0' : 'translate-x-full'}`}
+      >
         <div className="flex flex-col gap-16 pt-60 px-20">
           <div className="w-[254px] h-[24px] flex items-center justify-end">
             <button onClick={onClose} className="">
@@ -19,10 +32,13 @@ const HamburgerBar = ({ onClose }: { onClose: () => void }) => {
             <button className="w-[254px] py-[12px] pl-[16px] pr-[8px] rounded-8 text-left hover:bg-grey-800 hover:text-white cursor-pointer transition-all duration-200">
               데브톡 소개
             </button>
-            <button className="w-[254px] py-[12px] pl-[16px] pr-[8px] rounded-8 text-left hover:bg-grey-800 hover:text-white cursor-pointer transition-all duration-200">
+            <button
+              className="w-[254px] py-[12px] pl-[16px] pr-[8px] rounded-8 text-left hover:bg-grey-800 hover:text-white cursor-pointer transition-all duration-200"
+              onClick={() => navigate('/seminar')}
+            >
               세미나
             </button>
-            <button className="w-[254px] py-[12px] pl-[16px] pr-[8px] rounded-8 text-left hover:bg-grey-800 cursor-pointer transition-all duration-200">
+            <button className="w-[254px] py-[12px] pl-[16px] pr-[8px] rounded-8 text-left hover:bg-grey-800 cursor-pointer transition-all duration-200" onClick={() => navigate('/seminar/apply-info')}>
               <p className="text-gradient">n회차 데브톡 신청하기</p>
             </button>
             <hr className="border-gray-700 mt-[28px] mb-[8px]" />
--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -1,24 +1,26 @@
 import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import hamburger from '../../assets/icons/components/Header/hamburger.svg';
 import devlogo from '../../assets/logos/devlogo.svg';
 import HamburgerBar from './HamburgerBar';
 
 const Header = () => {
+  const navigate = useNavigate();
   const [hamburgerOpen, setHamburgerOpen] = useState(false);
 
   return (
-    <header className="relative flex justify-center items-center w-full h-[56px] bg-black">
+    <header className="flex justify-center items-center w-full h-[56px] bg-black">
       <div className="flex items-center justify-between w-[335px] h-[34px]">
-        <img src={devlogo} alt="devlogo" className="w-[77px] h-[32px]" />
+        <img src={devlogo} alt="devlogo" className="w-[77px] h-[32px] cursor-pointer" onClick={() => navigate('/')}/>
         <img
           src={hamburger}
           alt="hamburger"
           className="w-[24px] h-[24px] cursor-pointer"
-          onClick={() => setHamburgerOpen((prev) => !prev)}
+          onClick={() => setHamburgerOpen(true)}
         />
       </div>
 
-      {hamburgerOpen && <HamburgerBar onClose={() => setHamburgerOpen(false)} />}
+      <HamburgerBar isOpen={hamburgerOpen} onClose={() => setHamburgerOpen(false)} />
     </header>
   );
 };
