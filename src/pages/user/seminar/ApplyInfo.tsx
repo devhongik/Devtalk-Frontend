cf3d9b1b0f6c77184992071c4e084f2a6e6daeb5
YuminPark <p353700@gmail.com>
2025-09-30 03:05:31 +0900

    #83 [MOD] Chip에 w-fit 및 inline-flex 속성 적용

--- src/pages/user/seminar/ApplyInfo.tsx
+++ src/pages/user/seminar/ApplyInfo.tsx
@@ -1,7 +1,18 @@
 import ApplyHeader from '../../../components/SeminarApply/ApplyHeader';
+import { Chip } from '../../../components/Chip/Chip';
 
 const ApplyInfo = () => {
-  return <ApplyHeader backTo="/seminar/:id" />;
+  return (
+    <>
+      <ApplyHeader backTo="/seminar/:id" />
+      <div className="flex flex-col gap-48">
+        {/* Outline 영역 */}
+        <Chip text="Outline" />
+        {/* 온라인 LIVE 안내 영역 */}
+        <Chip text="온라인 LIVE 안내" />
+      </div>
+    </>
+  );
 };
 
 export default ApplyInfo;
