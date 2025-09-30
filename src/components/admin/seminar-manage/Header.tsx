4d672a2d14a7bd67e832beb94d635e4825db793c
Junseok Ahn <rrvvy9595@gmail.com>
2025-09-30 23:42:33 +0900

    #28 [FIX] 헤더 여백 문제 해결

--- src/components/admin/seminar-manage/Header.tsx
+++ src/components/admin/seminar-manage/Header.tsx
@@ -5,18 +5,16 @@ interface HeaderProps {
 }
 
 const Header = ({ title, showDeleteButton, onDelete }: HeaderProps) => (
-  <header className="sticky top-0 z-20 h-[70px] mb-10 bg-background/90">
-    <div className="max-w-[1030px] min-w-[850px] h-full mx-auto flex justify-between items-center">
-      <h1 className="heading-1-bold text-white">{title}</h1>
-      {showDeleteButton && (
-        <button
-          className="heading-3-semibold text-status-error hover:text-status-error/80 cursor-pointer"
-          onClick={onDelete}
-        >
-          세미나 삭제하기
-        </button>
-      )}
-    </div>
+  <header className="sticky top-0 z-20 h-[70px] mb-10 bg-background/90 mx-auto flex justify-between items-center min-w-[850px] max-w-[1030px]">
+    <h1 className="heading-1-bold text-white">{title}</h1>
+    {showDeleteButton && (
+      <button
+        className="heading-3-semibold text-status-error hover:text-status-error/80 cursor-pointer"
+        onClick={onDelete}
+      >
+        세미나 삭제하기
+      </button>
+    )}
   </header>
 );
 
