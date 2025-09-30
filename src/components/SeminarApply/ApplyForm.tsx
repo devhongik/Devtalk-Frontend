d5611f885996df4916cf93ad46658d6f17f79ae6
YuminPark <p353700@gmail.com>
2025-09-30 20:09:57 +0900

    #83 [FEAT] 학과 선택란 구현

--- src/components/SeminarApply/ApplyForm.tsx
+++ src/components/SeminarApply/ApplyForm.tsx
@@ -1,5 +1,14 @@
 import checkbox from '../../assets/icons/components/SeminarApply/checkbox.svg';
-import emptybox from '../../assets/icons/components/SeminarApply/empty.svg';
+import emptybox from '../../assets/icons/components/SeminarApply/emptybox.svg';
+
+const DEPARTMENTS = [
+  '컴퓨터공학과',
+  '산업데이터공학과',
+  '전자전기공학과',
+  '경영학과',
+  '산업디자인과',
+  '자율전공',
+];
 
 const ApplyForm = () => {
   return (
@@ -45,13 +54,50 @@ const ApplyForm = () => {
         </div>
       </div>
       {/* 학과 선택란 */}
-      <div className="flex flex-col gap-4">
-        <div className="flex flex-row gap-4">
-          <p className="heading-3-semibold text-white">학과를 선택해주세요</p>
-          <p className="heading-3-semibold text-gradient">*</p>
+      <div className="flex flex-col gap-20">
+        <div className="flex flex-col gap-4">
+          <div className="flex flex-row gap-4">
+            <p className="heading-3-semibold text-white">학과를 선택해주세요</p>
+            <p className="heading-3-semibold text-gradient">*</p>
+          </div>
+          <p className="body-2-medium text-grey-300">복수전공의 경우 모두 선택</p>
         </div>
-        <p className="body-2-medium text-grey-300">복수전공의 경우 모두 선택</p>
+
+        {/* 일반 학과들 */}
+        <div className="flex flex-col gap-20">
+          {DEPARTMENTS.map((dept, idx) => {
+            const id = `dept-${idx}`;
+            return (
+              <label key={id} htmlFor={id} className="flex flex-row gap-12 cursor-pointer">
+                <input id={id} type="checkbox" value={dept} className="sr-only peer" />
+                <img src={emptybox} alt="empty" className="w-6 h-6 peer-checked:hidden" />
+                <img src={checkbox} alt="checked" className="w-6 h-6 hidden peer-checked:block" />
+                <span className="body-1-medium text-white">{dept}</span>
+              </label>
+            );
+          })}
+
+          {/* 기타 */}
+          <label htmlFor="dept-other" className="flex items-center gap-12 cursor-pointer">
+            <input id="dept-other" type="checkbox" className="sr-only peer" />
+
+            <img src={emptybox} alt="empty" className="w-6 h-6 peer-checked:hidden" />
+            <img src={checkbox} alt="checked" className="w-6 h-6 hidden peer-checked:block" />
+
+            <span className="body-1-medium text-white shrink-0">기타:</span>
+            <input
+              type="text"
+              className="flex-1 bg-transparent outline-none
+             body-1-medium text-white
+             border-b border-grey-900
+             opacity-0 peer-checked:opacity-100 peer-checked:pointer-events-auto
+             pointer-events-none transition-opacity
+             focus:border-grey-600"
+            />
+          </label>
+        </div>
       </div>
+      {/* 학년 선택란 */}
     </>
   );
 };
