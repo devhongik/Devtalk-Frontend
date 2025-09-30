7bd35269467edb9151b24958087c34b21e0cd0d7
YuminPark <p353700@gmail.com>
2025-09-30 20:25:22 +0900

    #83 [FEAT] 학년 선택란 구현

--- src/components/SeminarApply/ApplyForm.tsx
+++ src/components/SeminarApply/ApplyForm.tsx
@@ -1,5 +1,7 @@
 import checkbox from '../../assets/icons/components/SeminarApply/checkbox.svg';
 import emptybox from '../../assets/icons/components/SeminarApply/emptybox.svg';
+import chosencircle from '../../assets/icons/components/SeminarApply/chosencircle.svg';
+import emptycircle from '../../assets/icons/components/SeminarApply/emptycircle.svg';
 
 const DEPARTMENTS = [
   '컴퓨터공학과',
@@ -9,6 +11,8 @@ const DEPARTMENTS = [
   '산업디자인과',
   '자율전공',
 ];
+
+const GRADES = ['1학년', '2학년', '3학년', '4학년'];
 
 const ApplyForm = () => {
   return (
@@ -63,7 +67,6 @@ const ApplyForm = () => {
           <p className="body-2-medium text-grey-300">복수전공의 경우 모두 선택</p>
         </div>
 
-        {/* 일반 학과들 */}
         <div className="flex flex-col gap-20">
           {DEPARTMENTS.map((dept, idx) => {
             const id = `dept-${idx}`;
@@ -97,7 +100,68 @@ const ApplyForm = () => {
           </label>
         </div>
       </div>
+
       {/* 학년 선택란 */}
+      <div className="flex flex-col gap-20">
+        <div className="flex flex-row gap-4">
+          <p className="heading-3-semibold text-white">학년을 선택해주세요</p>
+          <p className="heading-3-semibold text-gradient">*</p>
+        </div>
+
+        <div className="flex flex-col gap-6">
+          {GRADES.map((g, i) => {
+            const id = `grade-${i}`;
+            return (
+              <label
+                key={id}
+                htmlFor={id}
+                className="group flex items-center gap-12 cursor-pointer"
+              >
+                <input id={id} name="grade" type="radio" value={g} className="sr-only" />
+
+                <span className="relative w-6 h-6 shrink-0">
+                  <img src={emptycircle} alt="" className="w-6 h-6" />
+                  <img
+                    src={chosencircle}
+                    alt=""
+                    className="w-3 h-3
+                   absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2
+                           hidden group-has-[:checked]:block"
+                  />
+                </span>
+
+                <span className="body-1-medium text-white">{g}</span>
+              </label>
+            );
+          })}
+
+          {/* 기타 */}
+          <label htmlFor="grade-other" className="group flex items-center gap-12 cursor-pointer">
+            <input id="grade-other" name="grade" type="radio" value="기타" className="sr-only" />
+
+            <span className="relative w-6 h-6 shrink-0">
+              <img src={emptycircle} alt="" className="w-6 h-6" />
+              <img
+                src={chosencircle}
+                alt=""
+                className="w-3 h-3 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2
+                       hidden group-has-[:checked]:block"
+              />
+            </span>
+
+            <span className="body-1-medium text-white shrink-0">기타:</span>
+            <input
+              type="text"
+              className="flex-1 bg-transparent outline-none
+             body-1-medium text-white
+             border-b border-grey-900
+             opacity-0 peer-checked:opacity-100 peer-checked:pointer-events-auto
+             pointer-events-none transition-opacity
+             focus:border-grey-600"
+            />
+          </label>
+        </div>
+      </div>
     </>
   );
 };
