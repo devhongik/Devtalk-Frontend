3989a53e8e3e9199c8d8b503f8f38cee9748457f
YuminPark <p353700@gmail.com>
2025-09-30 19:38:13 +0900

    #83 [ADD] checkbox 아이콘 추가

--- src/components/SeminarApply/ApplyForm.tsx
+++ src/components/SeminarApply/ApplyForm.tsx
@@ -1,45 +1,55 @@
 const ApplyForm = () => {
   return (
-    <div className="flex flex-col gap-11">
-      {/* 이름 */}
-      <div className="flex flex-col gap-12">
-        <div className="flex flex-row gap-4">
-          <p className="heading-3-semibold text-white">이름을 적어주세요</p>
-          <p className="heading-3-semibold text-gradient">*</p>
-        </div>
-        <input
-          className="bg-grey-800 w-full h-14 rounded-8 placeholder:text-grey-300 px-16
+    <>
+      <div className="flex flex-col gap-11">
+        {/* 이름 */}
+        <div className="flex flex-col gap-12">
+          <div className="flex flex-row gap-4">
+            <p className="heading-3-semibold text-white">이름을 적어주세요</p>
+            <p className="heading-3-semibold text-gradient">*</p>
+          </div>
+          <input
+            className="bg-grey-800 w-full h-14 rounded-8 placeholder:text-grey-300 px-16
              outline-none border border-transparent focus:border-grey-500"
-          placeholder="ex. 김홍익"
-        />
-      </div>
+            placeholder="ex. 김홍익"
+          />
+        </div>
 
-      {/* 연락처 */}
-      <div className="flex flex-col gap-12">
-        <div className="flex flex-row gap-4">
-          <p className="heading-3-semibold text-white">연락처를 적어주세요</p>
-          <p className="heading-3-semibold text-gradient">*</p>
-        </div>
-        <input
-          className="bg-grey-800 w-full h-14 rounded-8 placeholder:text-grey-300 px-16
+        {/* 연락처 */}
+        <div className="flex flex-col gap-12">
+          <div className="flex flex-row gap-4">
+            <p className="heading-3-semibold text-white">연락처를 적어주세요</p>
+            <p className="heading-3-semibold text-gradient">*</p>
+          </div>
+          <input
+            className="bg-grey-800 w-full h-14 rounded-8 placeholder:text-grey-300 px-16
              outline-none border border-transparent focus:border-grey-500"
-          placeholder="ex. 010-0000-0000"
-        />
-      </div>
+            placeholder="ex. 010-0000-0000"
+          />
+        </div>
 
-      {/* 학번 */}
-      <div className="flex flex-col gap-12">
+        {/* 학번 */}
+        <div className="flex flex-col gap-12">
+          <div className="flex flex-row gap-4">
+            <p className="heading-3-semibold text-white">학번을 적어주세요</p>
+            <p className="heading-3-semibold text-gradient">*</p>
+          </div>
+          <input
+            className="bg-grey-800 w-full h-14 rounded-8 placeholder:text-grey-300 px-16
+             outline-none border border-transparent focus:border-grey-500"
+            placeholder="ex. C012345"
+          />
+        </div>
+      </div>
+      {/* 학과 선택란 */}
+      <div className="flex flex-col gap-4">
         <div className="flex flex-row gap-4">
-          <p className="heading-3-semibold text-white">학번을 적어주세요</p>
+          <p className="heading-3-semibold text-white">학과를 선택해주세요</p>
           <p className="heading-3-semibold text-gradient">*</p>
         </div>
-        <input
-          className="bg-grey-800 w-full h-14 rounded-8 placeholder:text-grey-300 px-16
-             outline-none border border-transparent focus:border-grey-500"
-          placeholder="ex. C012345"
-        />
+        <p className="body-2-medium text-grey-300">복수전공의 경우 모두 선택</p>
       </div>
-    </div>
+    </>
   );
 };
 
