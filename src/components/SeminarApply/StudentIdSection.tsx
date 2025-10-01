0a24e735513338f93382837f783feafc04a133ba
YuminPark <p353700@gmail.com>
2025-10-02 02:37:20 +0900

    #103 [CHORE] 에러 색상 코드 변경 및 경계선 색상 적용

--- src/components/SeminarApply/StudentIdSection.tsx
+++ src/components/SeminarApply/StudentIdSection.tsx
@@ -20,17 +20,19 @@ export const StudentIdSection = () => {
   return (
     <div className="flex flex-col gap-12">
       <SectionHeader title="학번을 적어주세요" required />
-
-      {/* relative 컨테이너 */}
       <div className="relative">
         <input
           value={studentId}
           onChange={handleChange}
           placeholder="ex. C012345"
           className={`bg-grey-800 w-full h-14 rounded-8 placeholder:text-grey-300 px-16
-                      outline-none border border-transparent focus:border-grey-500`}
+                      outline-none border 
+                      ${error ? 'border-status-error' : 'border-transparent'}
+                      focus:${error ? 'border-status-error' : 'border-grey-500'}`}
         />
-        {error && <p className="absolute left-0 top-full mt-1 text-red-500 text-sm">{error}</p>}
+        {error && (
+          <p className="absolute left-0 top-full mt-1 text-status-error body-2-medium">{error}</p>
+        )}
       </div>
     </div>
   );
