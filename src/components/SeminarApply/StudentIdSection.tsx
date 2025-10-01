51a4011a77005c22f2b8a33c665023177bf0a9dd
YuminPark <p353700@gmail.com>
2025-10-02 02:25:43 +0900

    #103 [FEAT] 학번 유효성 검사 함수 구현 및 연결

--- src/components/SeminarApply/StudentIdSection.tsx
+++ src/components/SeminarApply/StudentIdSection.tsx
@@ -1,12 +1,25 @@
+import { useState } from 'react';
 import { SectionHeader } from '../../components/SeminarApply/SectionHeader';
+import { formatStudentId } from '../../utils/formatStudentId';
 
-export const StudentIdSection = () => (
-  <div className="flex flex-col gap-12">
-    <SectionHeader title="학번을 적어주세요" required />
-    <input
-      className="bg-grey-800 w-full h-14 rounded-8 placeholder:text-grey-300 px-16
-                 outline-none border border-transparent focus:border-grey-500"
-      placeholder="ex. C012345"
-    />
-  </div>
-);
+export const StudentIdSection = () => {
+  const [studentId, setStudentId] = useState('');
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const value = formatStudentId(e.target.value);
+    setStudentId(value);
+  };
+
+  return (
+    <div className="flex flex-col gap-12">
+      <SectionHeader title="학번을 적어주세요" required />
+      <input
+        value={studentId}
+        onChange={handleChange}
+        placeholder="ex. C012345"
+        className="bg-grey-800 w-full h-14 rounded-8 placeholder:text-grey-300 px-16
+                   outline-none border border-transparent focus:border-grey-500"
+      />
+    </div>
+  );
+};
