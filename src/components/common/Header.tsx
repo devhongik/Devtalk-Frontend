c102f57e025f4c0f35c47f80e2c7f202f26a4559
chaeyoon <codbs0722@gmail.com>
2025-10-02 21:00:57 +0900

    #118 [MOD] 햄버거바 CSS 수정

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -9,24 +9,25 @@ const Header = () => {
   const [hamburgerOpen, setHamburgerOpen] = useState(false);
 
   return (
-    <header className="flex justify-center items-center w-full h-[56px] bg-black">
-      <div className="flex items-center justify-between w-[335px] h-[34px]">
-        <img
-          src={devlogo}
-          alt="devlogo"
-          className="w-[77px] h-[32px] cursor-pointer"
-          onClick={() => navigate('/')}
-        />
-        <img
-          src={hamburger}
-          alt="hamburger"
-          className="w-[24px] h-[24px] cursor-pointer"
-          onClick={() => setHamburgerOpen(true)}
-        />
-      </div>
-
+    <>
+      <header className="relative flex justify-center items-center w-full h-[56px] z-60 bg-black">
+        <div className="flex items-center justify-between w-[335px] h-[34px]">
+          <img
+            src={devlogo}
+            alt="devlogo"
+            className="w-[77px] h-[32px] cursor-pointer"
+            onClick={() => navigate('/')}
+          />
+          <img
+            src={hamburger}
+            alt="hamburger"
+            className="w-[24px] h-[24px] cursor-pointer"
+            onClick={() => setHamburgerOpen((prev) => !prev)}
+          />
+        </div>
+      </header>
       <HamburgerBar isOpen={hamburgerOpen} onClose={() => setHamburgerOpen(false)} />
-    </header>
+    </>
   );
 };
 
