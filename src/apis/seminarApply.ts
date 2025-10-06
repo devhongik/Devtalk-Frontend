943787147631e80213d3de3442ff173d521623dd
YuminPark <p353700@gmail.com>
2025-10-07 01:41:26 +0900

    #140 [CHORE] userInstance -> publicInstance로 변경

--- src/apis/seminarApply.ts
+++ src/apis/seminarApply.ts
@@ -1,9 +1,9 @@
-import { userInstance } from '../apis/userInstance';
+import { publicInstance } from '../apis/userInstance';
 import type { SeminarApplyRequest, SeminarApplyResponse } from '../types/Applicants/seminarApply';
 
 export const postApplySeminar = async (
   data: SeminarApplyRequest
 ): Promise<SeminarApplyResponse> => {
-  const res = await userInstance.post<SeminarApplyResponse>('/user/seminars', data);
+  const res = await publicInstance.post<SeminarApplyResponse>('/user/seminars', data);
   return res.data;
 };
