2da8bd9538e7fe4fe8e25322fd4bcf1d94da9fb6
YuminPark <p353700@gmail.com>
2025-10-07 00:04:14 +0900

    #140 [CHORE] 오타 수정

--- src/pages/user/seminar/ApplyQuestion.tsx
+++ src/pages/user/seminar/ApplyQuestion.tsx
@@ -91,7 +91,7 @@ const ApplyQuestion = () => {
               <div className="heading-2-bold text-white">사전 질문란</div>
             </div>
             <div className="body-2-medium text-grey-300">
-              연사님께서 질문 선별 후, Q&amp;A 시간에 답변 드립니다.
+              연사님께서 질문 선별 후, Q&A 시간에 답변 드립니다.
             </div>
           </div>
 
