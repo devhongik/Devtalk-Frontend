62a01af694054eca315f54f5046df77d0011e80d
YuminPark <p353700@gmail.com>
2025-10-06 21:10:27 +0900

    #140 [REFACTOR] 세미나 신청 관련 정보 상수 분리

--- src/components/SeminarApply/ApplyForm.tsx
+++ src/components/SeminarApply/ApplyForm.tsx
@@ -10,26 +10,7 @@ import { ParticipateSection } from './ParticipateSection';
 import { useApplyFormValidation } from '../../hooks/useApplyFormValidation';
 import { EmailSection } from './EmailSection';
 import { useApplyDraft } from '../../stores/useApplyDraft';
-
-const DEPARTMENTS = [
-  '컴퓨터공학과',
-  '산업데이터공학과',
-  '전자전기공학과',
-  '경영학과',
-  '산업디자인과',
-  '자율전공',
-];
-const GRADES = ['1학년', '2학년', '3학년', '4학년'];
-const HOWTOKNOW = [
-  '지인',
-  '교수님',
-  '에브리타임',
-  '학과 공지방',
-  '학회/동아리 공지방',
-  '교내 포스터 / X배너',
-  '인스타그램',
-];
-const PARTICIPATE = ['오프라인', '온라인 Live (Q&A, 이벤트 참여 불가)'];
+import { DEPARTMENTS, GRADES, HOWTOKNOW, PARTICIPATE } from '../../constants/seminarApplyInfo';
 
 const ApplyForm = () => {
   const navigate = useNavigate();
